a7f98732a2c9aa302edf7f52ef91a4cd6e128969
marcj <cordovamarcjulius085@gmail.com>
2026-03-08 23:27:21 +0800

    support vercel and github pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/anniversary-site/",
+  base: process.env.VERCEL ? "/" : "/anniversary-site/",
   plugins: [react()],
 });
