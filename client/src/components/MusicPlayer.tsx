3fe41db606fb4b77d0aae656f8ea4016589a5bdf
marcj <cordovamarcjulius085@gmail.com>
2026-03-26 20:15:15 +0800

    update

--- client/src/components/MusicPlayer.tsx
+++ client/src/components/MusicPlayer.tsx
@@ -9,7 +9,7 @@ const MusicPlayer = () => {
   const audioSrc = useMemo(
     () =>
       encodeURI(
-        `${import.meta.env.BASE_URL}audio/Rico Blanco - Balisong (Lyrics).mp3`,
+        `${import.meta.env.BASE_URL}audio/Rico Blanco - Balisong Transformed 2016`,
       ),
     [],
   );
